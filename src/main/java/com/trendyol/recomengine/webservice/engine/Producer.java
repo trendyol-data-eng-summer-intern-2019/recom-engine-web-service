6f01b35d262647621fa7b4f51af659f97aa1376b
yasinuygun <yasinuygun@outlook.com>
2019-07-16 16:25:02 +0300

    Documentations for classes Producer and RecommendationRepository are added.

--- src/main/java/com/trendyol/recomengine/webservice/engine/Producer.java
+++ src/main/java/com/trendyol/recomengine/webservice/engine/Producer.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+/**
+ * Used for sending messages to Kafka cluster. Needed for configuring the app as a Kafka producer. Can produce a message
+ * to any desired topic. Required parameters like default topic are configured in the application.yml file.
+ */
 @Service
 public class Producer {
 
@@ -14,6 +18,11 @@ public class Producer {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    /**
+     * Sends message to the default topic configured in the application.yml file. If the message is successfully sent
+     * to the Kafka cluster, logs the produced message at INFO level.
+     * @param message Message string to be sent to Kafka cluster's default topic.
+     */
     public void sendMessage(String message) {
         logger.info(String.format("#### -> Producing message -> %s", message));
         this.kafkaTemplate.sendDefault(message);
--- src/main/java/com/trendyol/recomengine/webservice/repositories/RecommendationRepository.java
+++ src/main/java/com/trendyol/recomengine/webservice/repositories/RecommendationRepository.java
@@ -3,8 +3,20 @@ package com.trendyol.recomengine.webservice.repositories;
 import com.trendyol.recomengine.webservice.resource.Recommendation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+/**
+ * Used to fetch recommendations of a specific user from MongoDB. Extends interface MongoRepository that is needed to
+ * configure the Spring Boot application to retrieve data from MongoDB. Required confifgurations are made in the
+ * application.yml file.
+ */
 public interface RecommendationRepository extends MongoRepository<Recommendation, String> {
 
+    /**
+     * Searches for a document having the same _id with the method parameter in the specified collection of a specified
+     * MongoDB database which are configured in the application.yml file.
+     * @param _id Document id to be searched.
+     * @return If the document is found, converts document to a Recommendation object and returns it; returns null
+     * otherwise.
+     */
     Recommendation findBy_id(String _id);
 
 }
